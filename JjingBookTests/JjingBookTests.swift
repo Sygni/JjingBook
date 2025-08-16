6846d773c94f470968a2a374c99371d66661c56c
Sygni <sygni.grit@gmail.com>
2025-08-16 19:24:07 +0900

    Initial Commit

---
+++ JjingBookTests/JjingBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  JjingBookTests.swift
+//  JjingBookTests
+//
+//  Created by Jeongah Seo on 8/16/25.
+//
+
+import Testing
+@testable import JjingBook
+
+struct JjingBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
